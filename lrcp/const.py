ead48fbab14cb87fc56876f476b198a5b8594fda
lordralinc <46781434+lordralinc@users.noreply.github.com>
2022-04-28 19:36:25 +0300

    ``

--- lrcp/const.py
+++ lrcp/const.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent.parent
-CONFIG_FILE = '/etc/lrcp_config.toml'
+CONFIG_FILE = Path('/etc/lrcp_config.toml')
 
 VERSION = '1.0.0'
 DESCRIPTION = 'Панель управления сервером'
